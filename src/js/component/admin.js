57330d425f7ff82328512c91c3a05ed53ce3bba1
Eduardo Fuentes Velasco <efuentes85@gmail.com>
2019-12-08 22:05:31 -0300

    Cambios en Admin

--- src/js/component/admin.js
+++ src/js/component/admin.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { Button, Card, Accordion, Form, Row, Col, Spinner } from "react-bootstrap";
 
 export class Admin extends Component {
 	render() {
@@ -8,12 +9,75 @@ export class Admin extends Component {
 			<div className="container">
 				<div clasdName="row">
 					<h1>Panel de Administracion</h1>
-					<ul>
-						<li>Deshabilitar Jugador</li>
-						<li>Borrar Equipo</li>
-						<li>Carrusel Imagenes</li>
-						<li>Reset Password</li>
-					</ul>
+					<br />
+					<Accordion>
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} variant="link" eventKey="0">
+									Deshabilitar Jugador!
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="0">
+								<Card.Body>
+									<Form>
+										<Form.Group controlId="formBasicEmail">
+											<Row>
+												<Col>
+													<Form.Control
+														type="email"
+														placeholder="Buscar Jugador por su nick"
+													/>
+												</Col>
+												<Col>
+													<Button variant="primary" type="submit">
+														Buscar
+													</Button>
+												</Col>
+											</Row>
+										</Form.Group>
+										<Row>
+											<Col />
+
+											<Col>
+												<Form.Check
+													type="switch"
+													id="custom-switch"
+													label="Activar/Deshabilitar"
+												/>
+											</Col>
+											<Col>
+												<Button variant="primary" type="submit">
+													Confirmar
+												</Button>
+											</Col>
+											<Col />
+										</Row>
+									</Form>
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+						<Card />
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} variant="link" eventKey="2">
+									Carrusel Imagenes
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="2">
+								<Card.Body>Hello! Im another body</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} variant="link" eventKey="3">
+									Reset Password de Usuarios
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="3">
+								<Card.Body>Hello! Im another body</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+					</Accordion>
 				</div>
 			</div>
 		);
